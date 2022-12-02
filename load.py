8fd52028631453c9c25eabaf836c4dd98fe16fed
Túlio <tuliofalmeida@hotmail.com>
2022-12-02 22:07:16 +0100

    day2

--- load.py
+++ load.py
@@ -4,18 +4,9 @@ def get_data(day):
     
     """
     import os
-    loc_path = os.getcwd()
+    loc_path = os.getcwd().capitalize()
 
-    base_path_e = 'E:\GitHub\\aoc2022\\data'
-    base_path_g = 'G:\Meu Drive\GitHUB\\aoc2022\\data'
-    ex_name = '\\' + day + '_ex.txt'
-    data_name = '\\' + day + '_data.txt'
-
-    if loc_path.upper()[0] == 'E':
-        file_ex = open(base_path_e + ex_name, 'r')
-        file_data = open(base_path_e + data_name, 'r')
-    else:
-        file_ex = open(base_path_g + ex_name, 'r')
-        file_data = open(base_path_g + data_name, 'r')
+    file_ex = open(loc_path + '\\data\\' + day + '_ex.txt','r')
+    file_data = open(loc_path + '\\data\\' + day + '_data.txt','r')
 
     return file_ex, file_data
